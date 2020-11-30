2ba8b9c56912468af3db328f034f65d5a81aaff0
reohashimoto <doribler@gmail.com>
2020-11-30 11:31:46 +0900

    チャットの画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,7 +6,7 @@ class RoomsController < ApplicationController
     @room = Room.new(room_params)
     if @room.save
       redirect_to root_path
-    else 
+    else
       render :new
     end
   end
